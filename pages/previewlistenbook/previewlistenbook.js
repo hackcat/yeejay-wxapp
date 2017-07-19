c6b9010497dbe4d415a5bef71c50f3e6c5c8637d
Fuming <78086091@qq.com>
2017-07-19 14:55:55 +0800

    preview listenbook

--- pages/previewlistenbook/previewlistenbook.js
+++ pages/previewlistenbook/previewlistenbook.js
@@ -25,8 +25,8 @@ Page({
     // reader 0 书本  1 朗读 
     getApp().getCommentList(that.data.bookInfo.bookId, that.data.bookInfo.reader, that.data.commentPageNum, function (res) {
       if (res.payload.comments.length !== 0) {
-        res.payload.comments.forEach(function(element, index) {
-          res.payload.comments[index].ts = utils.formatTime( new Date(element.ts * 1000) );
+        res.payload.comments.forEach(function (element, index) {
+          res.payload.comments[index].ts = utils.formatTime(new Date(element.ts * 1000));
         }, this);
         that.setData({
           comments: res.payload.comments,
@@ -43,49 +43,49 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
   // 编辑 删除 操作
@@ -109,7 +109,7 @@ Page({
     if (event.detail.value.comment) {
       getApp().addComment(that.data.bookInfo.bookid, that.data.bookInfo.reader, event.detail.value.comment, function (res) {
         if (res.code == 0) {
-          res.payload.comment.ts = utils.formatTime( new Date(res.payload.comment.ts * 1000) );
+          res.payload.comment.ts = utils.formatTime(new Date(res.payload.comment.ts * 1000));
           let data = that.data.comments.concat(res.payload.comment);
           wx.showToast({
             title: '评论成功',
@@ -132,15 +132,58 @@ Page({
     }
   },
 
+  // 更多操作
+  moreAct: function (event) {
+    let that = this;
+    let bookId = event.target.dataset.bookid;
+    console.log(event);
+    wx.showActionSheet({
+      itemList: ['投稿', '删除'],
+      success: function (res) {
+        if (res.tapIndex == 0) {
+          wx.navigateTo({
+            url: '../agreement/agreement?bookId=' + that.data.bookInfo.bookId +
+            '&title=' + that.data.bookInfo.title +
+            '&author=' + that.data.bookInfo.author +
+            '&coverUrl=' + that.data.bookInfo.coverUrl +
+            '&actType=' + that.data.bookInfo.bookReader
+          })
+        } else if (res.tapIndex == 1) {
+          // 删除
+          let bookid = event.currentTarget.dataset.bookid;
+          wx.showModal({
+            title: '提示',
+            content: '确认删除?',
+            success: function (res) {
+              if (res.confirm) {
+                getApp().delBook(bookid, function (data) {
+                  console.log(data);
+                  wx.reLaunch({
+                    url: '../profile/profile'
+                  });
+                });
+              } else if (res.cancel) {
+                console.log('用户点击取消')
+              }
+            }
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res.errMsg);
+      }
+    })
+  },
+
   // 删除一本书
-  delBook: function(event){
+  delBook: function (event) {
     let bookid = event.currentTarget.dataset.bookid;
     wx.showModal({
       title: '提示',
       content: '确认删除?',
-      success: function(res) {
+      success: function (res) {
         if (res.confirm) {
-          getApp().delBook(bookid, function(data){
+          getApp().delBook(bookid, function (data) {
             console.log(data);
             wx.reLaunch({
               url: '../profile/profile'
@@ -181,15 +224,15 @@ Page({
   },
 
   // like动作
-  likeIt: function(event) {
+  likeIt: function (event) {
     let that = this;
-    if(that.data.bookInfoData.hasLiked == 1){
+    if (that.data.bookInfoData.hasLiked == 1) {
       that.data.bookInfoData.hasLiked = 0;
       that.data.bookInfoData.likeCnt = that.data.bookInfoData.likeCnt - 1;
       that.setData({
         bookInfo: that.data.bookInfoData
       });
-      getApp().likeAct(that.data.bookInfo.bookId, that.data.bookInfo.reader, 0, function(res){
+      getApp().likeAct(that.data.bookInfo.bookId, that.data.bookInfo.reader, 0, function (res) {
         console.log('取消点赞');
       });
     } else {
@@ -198,7 +241,7 @@ Page({
       that.setData({
         bookInfo: that.data.bookInfoData
       });
-      getApp().likeAct(that.data.bookInfo.bookId, that.data.bookInfo.reader, 1, function(res){
+      getApp().likeAct(that.data.bookInfo.bookId, that.data.bookInfo.reader, 1, function (res) {
         console.log('点赞成功');
       });
     }
