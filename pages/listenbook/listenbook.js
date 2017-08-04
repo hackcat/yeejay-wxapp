5b18b4748aa344fd10a0dd24f134189391ae24d5
Fuming <78086091@qq.com>
2017-08-04 21:34:55 +0800

    auto play Vices

--- pages/listenbook/listenbook.js
+++ pages/listenbook/listenbook.js
@@ -40,7 +40,7 @@ function autoPlay(autoPlayArray, index) {
         }
       });
     }
-    autoPlay(autoPlayArray, index+1);
+    autoPlay(autoPlayArray, index + 1);
   }, autoPlayArray[index].time);
 }
 
@@ -57,7 +57,9 @@ Page({
     isPlayingTime: {}, // settimeout
     autoPlay: [], // 自动播放
 
-    swiperTime: 5000, // 轮播图切换时间
+    // 自动切换轮播图设置
+    swiperTime: 2000, // 轮播图切换时间
+    isAutoSwiper: true, // 自动轮播
 
     bookId: {}, // 书本ID
     bookReader: {}, // 书本类型 
@@ -68,6 +70,30 @@ Page({
     showComment: false // 输入评论
   },
 
+  // 切换swiper
+  swiperItem: function (event) {
+    let that = this;
+    let pageId = event.detail.current - 1;
+    console.log(pageId);
+    console.log(that.data.pagesData[pageId].swiperTime);
+    if (pageId >= 0) {
+      that.setData({
+        swiperTime: that.data.pagesData[pageId].swiperTime
+      });
+      that.autoPlayVices(pageId);
+    } else {
+      that.setData({
+        swiperTime: 2000
+      });
+    }
+    if( !that.data.isAutoSwiper ){
+      that.setData({
+        isAutoSwiper: true
+      });
+    }
+
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -76,7 +102,7 @@ Page({
     that.data.bookId = options.bookId;
     that.data.bookReader = options.reader;
 
-    getApp().getAudio({bookId: options.bookId, reader: options.reader, needPages: 1}, function (data) {
+    getApp().getAudio({ bookId: options.bookId, reader: options.reader, needPages: 1 }, function (data) {
       if (data.code == 0) {
         console.log(data);
         that.data.pagesData = data.payload.bookReadingInfo.pages;
@@ -88,11 +114,12 @@ Page({
           if (that.data.pagesData[index].time == 0) {
             that.data.pagesData[index].swiperTime = 1000;
           } else {
-            that.data.pagesData[index].swiperTime = val.duration - 0 + 1000;
+            that.data.pagesData[index].swiperTime = that.data.pagesData[index].time + 200;
           }
           that.data.pagesData[index].playTime = 1;
           that.data.pagesData[index].playTimeFt = '00:00';
         }, this);
+
         that.setData({
           bookInfo: data.payload.bookReadingInfo,
           bookInfoData: data.payload.bookReadingInfo,
@@ -116,11 +143,9 @@ Page({
             });
           }
         });
-
         // 自动播放
         console.log(that.data.autoPlay);
         // autoPlay(that.data.autoPlay, 0);
-
       } else {
         console.log("error_code:" + data.msg);
       }
@@ -133,7 +158,7 @@ Page({
   onReady: function () {
     let that = this;
     // reader 0 书本  1 朗读 
-    getApp().getCommentList({bookId: that.data.bookId, reader: that.data.bookReader, pageNum: that.data.commentPageNum}, function (res) {
+    getApp().getCommentList({ bookId: that.data.bookId, reader: that.data.bookReader, pageNum: that.data.commentPageNum }, function (res) {
       console.log(that.data.autoPlay);
       if (res.payload.comments.length !== 0) {
         res.payload.comments.forEach(function (element, index) {
@@ -193,12 +218,19 @@ Page({
   //点击播放录音  
   onVicesTap: function (event) {
     let that = this;
+    let playingIndex = event.currentTarget.dataset.index;
     console.log(event.currentTarget.dataset.index);
-    timeMeter(that, event.currentTarget.dataset.index);
+
+    that.setData({
+      isAutoSwiper: false
+    });
+
+    timeMeter(that, playingIndex);
     // 判断播放的页码是否同一
-    if (that.data.pageIndex == event.currentTarget.dataset.index) {
+    if (that.data.pageIndex == playingIndex) {
       // 同一页面 暂停UI
-      that.data.pagesData[event.currentTarget.dataset.index].isPlaying = false;
+
+      that.data.pagesData[playingIndex].isPlaying = false;
       that.setData({
         pages: that.data.pagesData
       });
@@ -207,13 +239,61 @@ Page({
       if (that.data.pageIndex >= 0) {
         that.data.pagesData[that.data.pageIndex].isPlaying = false;
       }
-      that.data.pagesData[event.currentTarget.dataset.index].isPlaying = true;
+      that.data.pagesData[playingIndex].isPlaying = true;
       that.setData({
         pages: that.data.pagesData
       });
 
       // 更新播放页面pageIndex
-      that.data.pageIndex = event.currentTarget.dataset.index;
+      that.data.pageIndex = playingIndex;
+
+      // 清除 setTimeout
+      clearTimeout(that.data.isPlayingTime);
+      // 设置自动回复停播状态
+      that.data.isPlayingTime = setTimeout(function () {
+        that.data.pagesData[that.data.pageIndex].isPlaying = false;
+        // 修改播放状态
+        that.setData({
+          pages: that.data.pagesData,
+          pageIndex: -1
+        });
+      }, that.data.pagesData[that.data.pageIndex].time);
+    }
+
+    // 下载缓存到本地 并且播放 pagesData
+    wx.playVoice({
+      filePath: that.data.pagesData[that.data.pageIndex].audioUrl,
+      success: function (res) {
+        console.log(res);
+      }
+    });
+  },
+
+  //自动播放录音  
+  autoPlayVices: function (pageId) {
+    let that = this;
+    console.log(pageId);
+    let playingIndex = pageId;
+    timeMeter(that, playingIndex);
+    // 判断播放的页码是否同一
+    if (that.data.pageIndex == playingIndex) {
+      // 同一页面 暂停UI
+      that.data.pagesData[playingIndex].isPlaying = false;
+      that.setData({
+        pages: that.data.pagesData
+      });
+    } else {
+      // 不是同一页面 上一个页面关闭，这个页面开启
+      if (that.data.pageIndex >= 0) {
+        that.data.pagesData[that.data.pageIndex].isPlaying = false;
+      }
+      that.data.pagesData[playingIndex].isPlaying = true;
+      that.setData({
+        pages: that.data.pagesData
+      });
+
+      // 更新播放页面pageIndex
+      that.data.pageIndex = playingIndex;
 
       // 清除 setTimeout
       clearTimeout(that.data.isPlayingTime);
@@ -246,7 +326,7 @@ Page({
       that.setData({
         bookInfo: that.data.bookInfoData
       });
-      getApp().likeAct({bookId: that.data.bookInfo.bookId, reader: that.data.bookInfo.reader, act: 0}, function (res) {
+      getApp().likeAct({ bookId: that.data.bookInfo.bookId, reader: that.data.bookInfo.reader, act: 0 }, function (res) {
         console.log('取消点赞');
       });
     } else {
@@ -255,7 +335,7 @@ Page({
       that.setData({
         bookInfo: that.data.bookInfoData
       });
-      getApp().likeAct({bookId: that.data.bookInfo.bookId, reader: that.data.bookInfo.reader, act: 1}, function (res) {
+      getApp().likeAct({ bookId: that.data.bookInfo.bookId, reader: that.data.bookInfo.reader, act: 1 }, function (res) {
         console.log('点赞成功');
       });
     }
@@ -280,7 +360,7 @@ Page({
     let that = this;
     console.log(event.detail.value.comment);
     if (event.detail.value.comment) {
-      getApp().addComment({bookId: that.data.bookId, reader: that.data.bookReader, content: event.detail.value.comment}, function (res) {
+      getApp().addComment({ bookId: that.data.bookId, reader: that.data.bookReader, content: event.detail.value.comment }, function (res) {
         if (res.code == 0) {
           res.payload.comment.ts = utils.formatTime(new Date(res.payload.comment.ts * 1000));
           let data = that.data.comments.concat(res.payload.comment);
